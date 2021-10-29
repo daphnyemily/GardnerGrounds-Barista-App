3cceba3c2378d6ca574255539c58be48d94e804c
Daphny Barbosa <daphnyemily@gmail.com>
2021-10-29 10:11:54 -0400

    adding barista app

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+  url : 'mongodb+srv://devdaph:tPshcQuYG0ltFxR9@cluster0.h8uhl.mongodb.net/gardnerGrounds?retryWrites=true&w=majority', 
+  dbName: 'GardnerGrounds'
+};
---
+++ public/main.js
@@ -0,0 +1,45 @@
+
+
+const completedOrder = (e) => {
+  const id = e.target.id;
+  console.log(id)
+  const awaiting = "true";
+  const completed = "false";
+  fetch('barista', {
+    method: 'put',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      id: id,
+    }),
+  }).then(function (res) {
+    console.log(res)
+    console.log(res.body)
+    return res.json()
+  })
+  .then(function (res) {
+    window.speechSynthesis.speak(new SpeechSynthesisUtterance(res.value.size +" "+ res.value.beverage + " for " + res.value.customer));
+  })
+  window.location.reload();
+};
+
+
+document.querySelectorAll('.complete-order').forEach((e) => {
+  e.addEventListener('click', completedOrder);
+});
+
+const deleteAllCompleted = (e) => {
+  fetch('barista', {
+    method: 'delete',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      id: 'all',
+    }),
+  }).then(function () {
+    
+    window.location.reload();
+  });
+};
+
+document.querySelectorAll('.clear-all-completed').forEach((e) => {
+  e.addEventListener('click', deleteAllCompleted);
+});
